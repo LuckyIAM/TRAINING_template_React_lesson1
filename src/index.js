a7c7d137c98aaa082bdd6d0e87c33ec084c3a3e8
LuckyIAM <zavalisca.elena@gmail.com>
2022-11-01 17:28:48 +0300

    start React

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import "./index.css"
 
 ReactDOM.render(
-	<App/>,
-	document.querySelector("#root")
-);
+	<div className="wrapper">
+		<div className="card">=)</div>
+		<div className="card">O_o</div>
+		<div className="card">^_^</div>
+		<div className="card">=|_</div>
+	</div>,
+	document.querySelector('#root')
+)
